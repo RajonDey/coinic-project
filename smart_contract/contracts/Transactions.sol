144619e11a759623dd88357db2cdc3cb1567740f
Rajon Dey <rajon.dey@sjinnovation.com>
2022-05-06 21:47:24 +0600

    Connect with hardhat

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0; // solidity version 
+pragma solidity ^0.8.10; // solidity version 
 
 contract Transactions {
     uint256 transactionCount; 
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,15 @@
+// https://eth-ropsten.alchemyapi.io/v2/zTbs-ng7RJ3ryk6zGqTSkHvlbJPnk72S
+
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.10",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/zTbs-ng7RJ3ryk6zGqTSkHvlbJPnk72S",
+      accounts: [
+        "9b426f3e14647cab4c2675ba7519c32d4e44e548de9ab6f6cc50258e36459ce6",
+      ],
+    },
+  },
 };
